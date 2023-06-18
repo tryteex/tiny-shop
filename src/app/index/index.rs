3a84d9570feef4ec925e361701d91142607edfe5
Volodymyr Zamkovyi <v.zamkovyi@gmail.com>
2023-06-18 17:53:08 +0300

    Some fix

--- src/app/index/index.rs
+++ src/app/index/index.rs
@@ -4,12 +4,12 @@ pub async fn index(this: &mut Action) -> Answer {
     this.load("head", "index", "main", "head", None).await;
     this.load("index", "index", "main", "index", None).await;
     this.load("foot", "index", "main", "foot", None).await;
-    this.render("index").await
+    this.render("index")
 }
 
 pub async fn not_found(this: &mut Action) -> Answer {
     this.load("head", "index", "main", "head", None).await;
     this.load("index", "index", "main", "not_found", None).await;
     this.load("foot", "index", "main", "foot", None).await;
-    this.render("index").await
+    this.render("index")
 }
